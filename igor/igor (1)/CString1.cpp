4af576bd525011df0fb01ce1cd9817e5cb5ce102
IgorPanarin-2000 <70935398+IgorPanarin-2000@users.noreply.github.com>
2021-03-25 12:30:24 +0300

    Add files via upload

---
+++ igor/igor (1)/CString1.cpp
@@ -0,0 +1,21 @@
+#include "CString1.h"
+
+CString1 operator+(const CString1 &l, const CString &r) {
+    CString1 a = l;
+    a += r;
+    return a;
+}
+
+int CString1::output() {
+    if(file == nullptr){
+        return 1;
+    }
+    std::ofstream out(file,std::ios::app);
+    for(size_t i = 1; i < getSize(); i++){
+        out << str[i] << std::endl;
+    }
+    out << std::endl;
+    out.close();
+    return 0;
+}
+
