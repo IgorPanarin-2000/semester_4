7a0811088fa234fd2ada83f7189ea80ab09fe494
IgorPanarin-2000 <70935398+IgorPanarin-2000@users.noreply.github.com>
2021-03-25 12:32:05 +0300

    Add files via upload

---
+++ igor (1)/CString0.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "CString.h"
+
+class CString0 : public CString{
+public:
+    using CString::operator=;
+    using CString::CString;
+    int output() override;
+
+    CString0(const char* inp = "", const char* FileName = nullptr) : CString(inp, FileName) {}
+    CString0(const CString& r) : CString(r) { }
+    CString0(CString&& r) : CString(std::move(r)) { }
+    ~CString0() {}
+
+    friend CString0 operator+(const CString0& l, const CString& r);
+};
