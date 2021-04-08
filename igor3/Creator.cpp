0053abe12781b541642560ed5f678db44f10e1da
IgorPanarin-2000 <70935398+IgorPanarin-2000@users.noreply.github.com>
2021-04-08 11:52:27 +0300

    Delete igor3 directory

--- igor3/Creator.cpp
+++
@@ -1,9 +0,0 @@
-#include "Creator.h"
-
-CString* CreatorString0::create(std::string inp, std::string file) {
-    return new CString0(inp, file);
-}
-
-CString* CreatorString1::create(std::string inp, std::string file) {
-    return new CString1(inp, file);
-}
